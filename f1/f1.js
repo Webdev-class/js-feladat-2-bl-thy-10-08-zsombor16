3ae28493e1fe3542fb01021502e14463f06cca12
zsombor16 <zsomborg432@gmail.com>
2024-10-08 08:37:33 +0200

    kesz

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,14 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+   
+
+        const response = await fetch(`https://archive-api.open-meteo.com/v1/archive?latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`);
+        const data = await response.json();
+
+        const temperatures = data.hourly.temperature_2m;
+        const averageTemperature = Math.floor(temperatures.reduce((a, b) => a + b, 0) / temperatures.length);
+        return averageTemperature;
 }
 
 module.exports = f1;
